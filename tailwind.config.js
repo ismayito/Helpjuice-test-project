6ff0f08ff78112d7f5e55c00446081da5a5263b4
Ismail mayito <ismayito1@gmail.com>
2024-03-14 11:18:06 +0300

    Add tailwind preprocessor

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+};
